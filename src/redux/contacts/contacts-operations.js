68c67e4e4b6bca0b0ab6ed0d2247d2496da2e04f
MariiaMelychyn <lv080191mmi75@gmail.com>
2021-11-16 17:50:06 +0200

    dispatch

--- src/redux/contacts/contacts-operations.js
+++ src/redux/contacts/contacts-operations.js
@@ -1,43 +1,97 @@
-import { createAsyncThunk } from '@reduxjs/toolkit';
+// import { createAsyncThunk } from '@reduxjs/toolkit';
+// import axios from 'axios';
+
+// axios.defaults.baseURL = 'https://6190edc741928b001768fedf.mockapi.io';
+
+// export const fetchContacts = createAsyncThunk(
+//   'contacts/fetchContacts',
+//   async () => {
+//     try {
+//       const { data } = await axios.get('/contacts');
+//       return data;
+//     } catch (error) {
+//       console.log(error.message);
+//     }
+//   },
+// );
+
+// export const addContact = createAsyncThunk(
+//   'contacts/addContact',
+//   async contact => {
+//     try {
+//       const { data } = await axios.post('/contacts', contact);
+//       return data;
+//     } catch (error) {
+//       console.log(error.message);
+//     }
+//   },
+// );
+
+// export const deleteContact = createAsyncThunk(
+//   'contacts/deleteContact',
+//   async contactId => {
+//     try {
+//       const {
+//         data: { id },
+//       } = await axios.delete(`/contacts/${contactId}`);
+
+//       return id;
+//     } catch (error) {
+//       console.log(error.message);
+//     }
+//   },
+// );
+
+
+
 import axios from 'axios';
+import { toast } from 'react-toastify';
 
-axios.defaults.baseURL = 'https://6190edc741928b001768fedf.mockapi.io';
+import actions from './contacts-actions'; // Импорт экшенов из контактов в операции
 
-export const fetchContacts = createAsyncThunk(
-  'contacts/fetchContacts',
-  async () => {
-    try {
-      const { data } = await axios.get('/contacts');
-      return data;
-    } catch (error) {
-      console.log(error.message);
-    }
-  },
-);
+// Асинхронная операция получения списка контактов (делает запрос, диспатчит экшн получения)
+export const fetchContacts = () => async dispatch => {
+  dispatch(actions.fetchContactsRequest());
 
-export const addContact = createAsyncThunk(
-  'contacts/addContact',
-  async contact => {
-    try {
-      const { data } = await axios.post('/contacts', contact);
-      return data;
-    } catch (error) {
-      console.log(error.message);
-    }
-  },
-);
+  try {
+    const { data } = await axios.get('/contacts');
 
-export const deleteContact = createAsyncThunk(
-  'contacts/deleteContact',
-  async contactId => {
-    try {
-      const {
-        data: { id },
-      } = await axios.delete(`/contacts/${contactId}`);
+    dispatch(actions.fetchContactsSuccess(data));
+  } catch (error) {
+    dispatch(actions.fetchContactsError(error.message));
+    toast.error(error.message);
+  }
+};
 
-      return id;
-    } catch (error) {
-      console.log(error.message);
-    }
-  },
-);
+// Асинхронная операция по добавлению контакта (принимает данные с формы, делает запрос, диспатчит экшн добавления)
+export const addContact = (name, number) => async dispatch => {
+  const contact = { name, number };
+
+  dispatch(actions.addContactRequest());
+
+  try {
+    const { data } = await axios.post('/contacts', contact);
+
+    toast.success('Added');
+    dispatch(actions.addContactSuccess(data));
+  } catch (error) {
+    dispatch(actions.addContactError(error.message));
+    toast.error(error.message);
+  }
+};
+
+// Асинхронная операция удаления контакта (принимает id, делает запрос, диспатчит экшн удаления)
+export const deleteContact = id => async dispatch => {
+  dispatch(actions.deleteContactRequest());
+
+  try {
+    await axios.delete(`/contacts/${id}`);
+
+    toast.success('Deleted');
+    dispatch(actions.deleteContactSuccess(id));
+  } catch (error) {
+    dispatch(actions.deleteContactError(error.message));
+    toast.error(error.message);
+  }
+};
+
