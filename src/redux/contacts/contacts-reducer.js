586ed6a4429512f5371bc83c62fff599a732adb8
MariiaMelychyn <lv080191mmi75@gmail.com>
2021-11-17 20:06:09 +0200

    Update contacts-reducer.js

--- src/redux/contacts/contacts-reducer.js
+++ src/redux/contacts/contacts-reducer.js
@@ -14,44 +14,53 @@ const contactsSlice = createSlice({
     [fetchContacts.fulfilled]: (state, action) => ({
       ...state,
       contactItems: action.payload,
-      loading: false
+      loading: false,
+      error: null,
     }),
     [fetchContacts.pending]: state => ({
       ...state,
-      loading: true
+      loading: true,
+      error: null,
     }),
     [fetchContacts.rejected]: (state, action) => ({
       ...state,
-      loading: false
+      loading: false,
+      error: action.payload,
     }),
 
     [addContact.fulfilled]: (state, action) => ({
       ...state,
       contactItems: [...state.contactItems, action.payload],
-      loading: false
+      loading: false,
+      error: null,
     }),
     [addContact.pending]: state => ({
       ...state,
-      loading: true
+      loading: true,
+      error: null,
     }),
     [addContact.rejected]: (state, action) => ({
       ...state,
-      loading: false
+      loading: false,
+      error: action.payload,
     }),
 
     [deleteContact.fulfilled]: (state, action) => ({
       ...state,
       contactItems: state.contactItems.filter(
-        ({ id }) => id !== action.payload,),
-      loading: false
+        ({ id }) => id !== action.payload),
+      loading: false,
+      error: null,
     }),
     [deleteContact.pending]: state => ({
       ...state,
-      loading: true
+      loading: true,
+      error: null,
     }),
     [deleteContact.rejected]: (state, action) => ({
       ...state,
-      loading: false
+      loading: false,
+      error: action.payload,
     }),
   },
 });
